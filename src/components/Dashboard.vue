c5dfe25f8d915bcf3b8700cdc072c05f58e6b16a
lida <northloi@163.com>
2024-08-14 18:35:56 +0800

    feat: router

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import BoardItem from './BoardItem.vue'
+</script>
+
+<template>
+  <BoardItem>
+    <template #icon> </template>
+    <template #heading>welcome</template>
+  </BoardItem>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h3>vue3 web app</h3>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
